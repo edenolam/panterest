4b18e3bf26d03a108e150b61c92e2eae0da71838
edenolam <julienbasquin.jb@gmail.com>
2020-09-01 18:15:01 +0200

    add show page and links to pages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +13,23 @@ class PinsController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @Route("/", name="app_pins_index")
      * @param PinRepository $pinRepository
      * @return Response
      */
     public function index(PinRepository $pinRepository):Response
     {
-        return $this->render('pins/index.html.twig', ['pins'=> $pinRepository->findAll()]);
+        $pins = $pinRepository->findAll();
+        return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * @param Pin $pin
+     * @return Response
+     */
+    public function show(Pin $pin):Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
